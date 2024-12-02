4026731facc6573638ec492bff47e21c6bedca67
Vengatesan301 <kit.24.20bcs057@gmail.com>
2024-12-02 19:55:45 +0530

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -36,12 +36,11 @@ const Body = () => {
 
 
   const fetchData = async () => {
-    // const data = await fetch(
-    //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.1197626&lng=80.1904968&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    // );
     const data = await fetch(
-      "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.1197626&lng=80.1904968&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.1197626&lng=80.1904968&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+   
+    
   
     const json = await data.json();
   
